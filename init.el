1d4e294c204092a46a0b35ebcbd25dc90c05e06a
Deirdre Connolly <durumcrustulum@gmail.com>
2012-07-09 15:21:16 -0400

    Sass mode

--- init.el
+++ init.el
@@ -44,3 +44,38 @@
 
 ;; Disable line numbering
 (global-linum-mode 0)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Malabar Mode
+;; https://github.com/espenhw/malabar-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
+                                      global-semanticdb-minor-mode
+                                      global-semantic-idle-summary-mode
+                                      global-semantic-mru-bookmark-mode))
+(semantic-mode 1)
+(require 'malabar-mode)
+(setq malabar-groovy-lib-dir "~/.emacs.d/malabar-mode/target/malabar-1.4.0/lib")
+(add-to-list 'auto-mode-alist '("\\.java\\'" . malabar-mode))
+
+;; (add-hook 'malabar-mode-hook
+;;      (lambda () 
+;;        (add-hook 'after-save-hook 'malabar-compile-file-silently
+;;                   nil t)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Coffee Mode
+;; https://github.com/defunkt/coffee-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Sass Mode
+;; https://github.com/nex3/sass-mode
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'sass-mode)
