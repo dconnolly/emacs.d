e8edd16a475df42455fcbc3313b53349c0e53d49
Deirdre Connolly <dconnolly@hubspot.com>
2012-09-07 22:44:42 -0400

    auto modes for sass/scss

--- init.el
+++ init.el
@@ -79,3 +79,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (require 'sass-mode)
+(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$" . sass-mode))
