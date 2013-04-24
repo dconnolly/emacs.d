55afc2475414812dd0860ed4170f25dce67952e4
Deirdre Connolly <durumcrustulum@gmail.com>
2013-04-24 14:41:54 -0400

    Remove things for pycheckers - now handled by flake8 via pycheckers, and possibly redundant tab/spaces config

--- modes/py.el
+++ modes/py.el
@@ -8,11 +8,10 @@
 ;; TODO: Replace all emacs-for-python shit with installable packages until 
 
 ;; (require 'epy-init)
-;; (epy-setup-checker "~/.emacs.d/etc/pycheckers.sh %f")
 
 ;;; Disable tab characters, set default tab spacing to 4 chars wide.
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
+;; (setq-default indent-tabs-mode nil)
+;; (setq-default tab-width 4)
 
 (provide 'py)
 ;;; py.el ends here
