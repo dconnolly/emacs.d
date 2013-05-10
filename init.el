9bf71f58c41e268bbe39242807ca227d2b65043e
Deirdre Connolly <durumcrustulum@gmail.com>
2013-05-10 12:44:18 -0400

    Allow emacs to edit OSX plist files

--- init.el
+++ init.el
@@ -100,6 +100,23 @@
 
 (add-hook 'java-mode-hook (lambda () (require 'java)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; OSX plist bullshit
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Allow editing of binary .plist files.
+(add-to-list 'jka-compr-compression-info-list
+	     ["\\.plist$"
+	      "converting text XML to binary plist"
+	      "plutil"
+	      ("-convert" "binary1" "-o" "-" "-")
+	      "converting binary plist to text XML"
+	      "plutil"
+	      ("-convert" "xml1" "-o" "-" "-")
+	      nil nil "bplist"])
+
+;;It is necessary to perform an update!
+(jka-compr-update)
 
 (provide 'init)
 ;;; init.el ends here
