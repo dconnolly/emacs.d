152f45de430628125589996f9ee9e8c0035238c3
Deirdre Connolly <durumcrustulum@gmail.com>
2014-09-18 14:01:44 -0400

    Change tag, attr, and bracket face colors for web-mode to make them more legible

---
+++ modes/web.el
@@ -0,0 +1,3 @@
+(set-face-attribute 'web-mode-html-tag-face nil :foreground "Snow4")
+(set-face-attribute 'web-mode-html-attr-name-face nil :foreground "Snow3")
+(set-face-attribute 'web-mode-html-tag-bracket-face nil :foreground "Snow3")
